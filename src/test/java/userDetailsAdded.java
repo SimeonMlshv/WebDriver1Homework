cbe8d9355a1c32e6989f77f2feb7323c4755d6af
SimeonMlshv <simeon.miloshov.a61@learn.telerikacademy.com>
2024-09-30 16:06:43 +0200

    Updating the 2nd test

---
+++ src/test/java/userDetailsAdded.java
@@ -0,0 +1,49 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class userDetailsAdded {
+
+    @Test
+    public void addUserDetailsAtCheckout() {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.saucedemo.com/v1/");
+
+        WebElement username = driver.findElement(By.id("user-name"));
+        username.sendKeys("standard_user");
+
+        WebElement password = driver.findElement(By.id("password"));
+        password.sendKeys("secret_sauce" + Keys.ENTER);
+
+        WebElement firstItemAdd = driver.findElement(By.xpath("//*[@id=\"inventory_container\"]/div/div[1]/div[3]/button"));
+        firstItemAdd.click();
+
+        WebElement secondItemAdd = driver.findElement(By.xpath("//*[@id=\"inventory_container\"]/div/div[2]/div[3]/button"));
+        secondItemAdd.click();
+
+        WebElement shoppingCart = driver.findElement(By.id("shopping_cart_container"));
+        shoppingCart.click();
+
+        WebElement checkout = driver.findElement(By.xpath("//*[@id=\"cart_contents_container\"]/div/div[2]/a[2]"));
+        checkout.click();
+
+        WebElement firstName = driver.findElement(By.id("first-name"));
+        firstName.sendKeys("Simeon");
+        WebElement lastName = driver.findElement(By.id("last-name"));
+        lastName.sendKeys("Miloshov");
+        WebElement postCode = driver.findElement(By.id("postal-code"));
+        postCode.sendKeys("2600");
+
+        WebElement continueButton = driver.findElement(By.xpath("//*[@id=\"checkout_info_container\"]/div/form/div[2]/input"));
+        continueButton.click();
+
+        var summaryPageOverview = driver.findElement(By.xpath("//*[@id=\"contents_wrapper\"]/div[2]"));
+        Assertions.assertEquals("Checkout: Overview", summaryPageOverview.getText());
+    }
+}
